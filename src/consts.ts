829dded1fa900536c78cc17d916b6c7880e1005c
John Serrano <andrey_201015@hotmail.com>
2023-03-19 16:41:49 -0500

    Add header and css global

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Desarrollador web - John Serrano | Frontend, JavaScript, React, Node.js';
+export const SITE_DESCRIPTION = 'Desarrollador Web Full-Stack un apasionado y entusiasta de las tecnología web: JavaScript, Node.js, Docker, Firebase, React, etc..';
