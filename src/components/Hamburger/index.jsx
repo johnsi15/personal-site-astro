b12a2a86b05f0ed81d845f8d0c6493987b01efd9
John Serrano <andrey_201015@hotmail.com>
2023-03-23 17:19:08 -0500

    Changes hamburger and nav#2

---
+++ src/components/Hamburger/index.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback } from 'react'
+import styles from './Hamburger.module.css'
+
+// const { active, handleClick } = Astro.props
+const Hamburger = () => {
+  const [active, setActive] = useState(false) // Menu
+
+  const handleClick = useCallback(() => {
+    setActive(!active)
+  }, [active])
+
+  return (
+    <button
+      aria-label='Hamburger'
+      id='Hamburger'
+      className={`${styles.hamburger} ${styles['hamburger--emphatic']} ${
+        active ? styles['is-active'] : ''
+      }`}
+      type='button'
+      onClick={handleClick}
+    >
+      <span className={styles['hamburger-box']}>
+        <span className={styles['hamburger-inner']} />
+      </span>
+    </button>
+  )
+}
+
+export default Hamburger
