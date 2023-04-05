b50acb8d02fd3d2e4045c0efb8597d2f7c77a3df
John Serrano <andrey_201015@hotmail.com>
2023-04-05 12:28:32 -0500

    Change favicon

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,20 @@
 
 export const SITE_TITLE = 'Desarrollador web - John Serrano | Frontend, JavaScript, React, Node.js';
 export const SITE_DESCRIPTION = 'Desarrollador Web Full-Stack un apasionado y entusiasta de las tecnología web: JavaScript, Node.js, Docker, Firebase, React, etc..';
+export const LANG = 'es'
+export const openGraph = {
+  type: 'website',
+  locale: 'es_CO',
+  url: 'https://johnserrano.co',
+  title: 'Desarrollador web - John Serrano',
+  description:
+    'Desarrollador Web Full-Stack un apasionado y entusiasta de las tecnología web: JavaScript, Node.js, Docker, Firebase, React, etc..',
+  image: 'https://johnserrano.co/default.jpg',
+  site_name: 'johnserrano.co',
+  imageWidth: 1200,
+  imageHeight: 630
+}
+export const twitter = {
+  handle: '@jandrey15',
+  cardType: 'summary_large_image'
+}
