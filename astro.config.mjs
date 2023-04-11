1d0a05d12d24b22e94f02c56acfa766cd65d5cf4
John Serrano <andrey_201015@hotmail.com>
2023-04-11 12:19:33 -0500

    Add GTM script with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react'
 // https://astro.build/config
 import vercel from '@astrojs/vercel/static'
 // DOC: https://docs.astro.build/es/guides/integrations-guide/vercel/
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +16,17 @@ export default defineConfig({
     drafts: true,
   },
   site: 'https://johnserrano.co',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   output: 'static',
   adapter: vercel({
     analytics: true,
