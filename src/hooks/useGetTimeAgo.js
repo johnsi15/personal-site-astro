7723f41b54561f9199b3a2bbb65cde5103d50d0e
John Serrano <andrey_201015@hotmail.com>
2023-05-13 11:26:15 -0500

    Change posts sort order

--- src/hooks/useGetTimeAgo.js
+++ src/hooks/useGetTimeAgo.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react'
-import { getTimeAgo, getDateHuman } from '../helpers/formattedDate'
+// import { getTimeDate } from '../helpers/formattedDate'
 
 function useGetTimeAgo({ publishedDate }) {
   const [timeAgo, setTimeAgo] = useState()
   const [publishedAt, setPublishedAt] = useState()
 
-  const timestamp = new Date(publishedDate).getTime()
-  // sacar este hook y volverlo un helper
-  useEffect(() => {
-    setTimeAgo(getTimeAgo(timestamp))
-    setPublishedAt(getDateHuman(timestamp))
-  }, [])
+  // const timestamp = new Date(publishedDate).getTime()
+  // // sacar este hook y volverlo un helper
+  // useEffect(() => {
+  //   setTimeAgo(timeAgo(timestamp))
+  //   setPublishedAt(publishedAt(timestamp))
+  // }, [])
 
-  return { timeAgo, publishedAt }
+  return { timeAgo }
 }
 
 export default useGetTimeAgo
