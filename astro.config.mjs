cb76ec1727af2e0fa1cc72f159f7ea176936f303
John Serrano <andrey_201015@hotmail.com>
2023-11-28 17:46:05 -0500

    Fix gtm partytown#7

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,10 +35,11 @@ export default defineConfig({
       // Adds dataLayer.push as a forwarding-event.
       config: {
         resolveUrl: (url, location, type) => {
+          console.log(url)
+          console.log(location)
           const proxiedHosts = [
             'googletagmanager.com',
-            'connect.facebook.net',
-            'pagead2.googlesyndication.com'
+            'connect.facebook.net'
           ]
 
           if (proxiedHosts.includes(url.hostname)) {
@@ -48,6 +49,10 @@ export default defineConfig({
             return proxyUrl;
           } else if (url.hostname === 'www.google-analytics.com') {
             const proxyUrl = new URL('/proxytown/ga', 'https://johnserrano.co');
+            proxyUrl.searchParams.append('url', url.href);
+            return proxyUrl;
+          } else if (url.hostname === 'pagead2.googlesyndication.com') {
+            const proxyUrl = new URL('/proxytown/pagead2', 'https://johnserrano.co');
             proxyUrl.searchParams.append('url', url.href);
             return proxyUrl;
           }
