a9605ad34613cd996336c82bea5394a1f0793a7e
John Serrano <andrey_201015@hotmail.com>
2024-05-24 16:56:30 -0500

    Change response time toast

--- src/helpers/toast.ts
+++ src/helpers/toast.ts
@@ -30,7 +30,7 @@ interface ButterupOptions {
 export const butterup: ButterupProps = {
   options: {
     maxToasts: 3,
-    toastLife: 3000,
+    toastLife: 7000,
     currentToasts: 0,
     stackedToasts: true,
   },
