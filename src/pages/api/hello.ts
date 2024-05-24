087fd66ba045558c59bd56bc10d97a42af982fe2
John Serrano <andrey_201015@hotmail.com>
2024-05-24 16:31:23 -0500

    Add headers test endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,11 +1,15 @@
-import type { APIRoute } from 'astro';
+import type { APIRoute } from 'astro'
 
 export const prerender = false
 
 export const GET: APIRoute = ({ params, request }) => {
-
-  return new Response(JSON.stringify({
-      path: new URL(request.url).pathname
-    })
+  return new Response(
+    JSON.stringify({
+      path: new URL(request.url).pathname,
+    }),
+    {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    }
   )
 }
