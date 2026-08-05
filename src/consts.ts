05c97a39b464f6099570021be1728bba69248dc7
John Serrano <andrey_201015@hotmail.com>
2026-08-04 20:58:53 -0500

    feat: actualizar experiencia profesional a más de 10 años en varias secciones; mejorar descripciones y claridad en el contenido

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = 'Desarrollador web - John Serrano | Frontend, JavaScript, TypeScript, React, Node.js'
 export const SITE_DESCRIPTION =
-  'Ingeniero de software con más de 7 años de experiencia, desarrollo web y entusiasta de tecnologías web como JavaScript, TypeScript, Node.js y React.js.'
+  'Ingeniero de software con más de 10 años de experiencia, desarrollo web y entusiasta de tecnologías web como JavaScript, TypeScript, Node.js y React.js.'
 
 export const LANG = 'es'
 export const openGraph = {
